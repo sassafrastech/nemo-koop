2cdf9c1de38da12537dd413f3e4c445782cbb900
Kevin Cooper <cooperka92@gmail.com>
2020-08-18 17:48:08 -0700

    :sparkles: Install basic GeoJson output

--- src/plugins.js
+++ src/plugins.js
@@ -1,8 +1,16 @@
+const koopOutputGeoJson = require("koop-output-geojson");
 const koopProviderNemo = require('koop-provider-nemo');
-const outputs = [];
+
+const outputs = [{
+  instance: koopOutputGeoJson
+}];
+
 const auths = [];
+
 const caches = [];
+
 const plugins = [{
   instance: koopProviderNemo
 }];
+
 module.exports = [...outputs, ...auths, ...caches, ...plugins];
