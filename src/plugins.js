d8343181b06ff422a51d8601f1bd5721da1d447a
Kevin Cooper <cooperka92@gmail.com>
2020-08-19 11:07:37 -0700

    :rocket: Add before hook to log requests

--- src/plugins.js
+++ src/plugins.js
@@ -10,7 +10,19 @@ const auths = [];
 const caches = [];
 
 const plugins = [{
-  instance: koopProviderNemo
+  instance: koopProviderNemo,
+  options: {
+    before: (request, callback) => {
+      const { method, url } = request;
+      // Log incoming requests for diagnostics.
+      console.debug(`-> ${method} ${url}`)
+      callback();
+    },
+    after: (request, geojson, callback) => {
+      // We can post-process here if needed.
+      callback(null, geojson);
+    }
+  }
 }];
 
 module.exports = [...outputs, ...auths, ...caches, ...plugins];
