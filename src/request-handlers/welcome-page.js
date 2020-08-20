02801366b2dad59a2ebab4afaed3a92998658a1f
Kevin Cooper <cooperka92@gmail.com>
2020-08-20 09:04:55 -0700

    :sparkles: Print example on welcome page

--- src/request-handlers/welcome-page.js
+++ src/request-handlers/welcome-page.js
@@ -1,5 +1,9 @@
+const koopProviderNemo = require('koop-provider-nemo');
+
+const FULL_EXAMPLE = koopProviderNemo.Model.FULL_EXAMPLE;
+
 function handleRequest(req, res) {
-  res.status(200).send('Welcome to Koop!');
+  res.status(200).send(`<p>Welcome to Koop! Example URL:<p>${FULL_EXAMPLE}`);
 }
 
 module.exports = handleRequest;
